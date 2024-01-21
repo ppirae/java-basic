c05a0ed24cd4956a2fd64f460c6470a5aeaa571f
ppirae <whgudfo77@gmail.com>
2024-01-21 21:08:54 +0900

    객체 지향 프로그래밍

--- src/ref/ex/ProductOrderMain2.java
+++ src/ref/ex/ProductOrderMain2.java
@@ -1,30 +1,12 @@
-package class1.ex;
+package ref.ex;
 
-import java.util.Scanner;
-
-public class ProductOrderMain3 {
+public class ProductOrderMain2 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("입력할 주문의 개수를 입력하세요: ");
-        int n = scanner.nextInt();
-        scanner.nextLine();
-
-        ProductOrder[] orders = new ProductOrder[n];
-        for (int i = 0; i < orders.length; i++) {
-            System.out.println((i+1) + "번째 주문 정보를 입력하세요.");
-            System.out.print("상품명: ");
-            String productName = scanner.nextLine();
-
-            System.out.print("가격: ");
-            int price = scanner.nextInt();
-
-            System.out.print("수량: ");
-            int quantity = scanner.nextInt();
-            scanner.nextLine(); //입력 버퍼를 비우기 위한 코드
-
-            orders[i] = createOrder(productName, price, quantity);
-        }
+        ProductOrder[] orders = new ProductOrder[3];
+        orders[0] = createOrder("두부", 2000, 2);
+        orders[1] = createOrder("김치", 5000, 1);
+        orders[2] = createOrder("콜라", 1500, 2);
 
         printOrder(orders);
         printTotalAmount(orders);
